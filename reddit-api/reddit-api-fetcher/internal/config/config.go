b6c19014b31dc5ab44f85df24f178ed504d236d9
ptisma <petar.tisma@outlook.com>
2023-05-05 01:04:23 +0200

    continue on fetcher

--- reddit-api/reddit-api-fetcher/internal/config/config.go
+++ reddit-api/reddit-api-fetcher/internal/config/config.go
@@ -1,6 +1,9 @@
 package config
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
 const BROKER_URI = "BROKER_URI"
 const TOPIC_NAME = "TOPIC_NAME"
@@ -19,5 +22,44 @@ func GetRabbitMQConfig() *RabbitMQConfig {
 	routingKey := os.Getenv(ROUTING_KEY)
 
 	return &RabbitMQConfig{BrokerURI: brokerURI, TopicName: topicName, RoutingKey: routingKey}
+
+}
+
+const REDDIT_USERNAME = "REDDIT_USERNAME"
+const REDDIT_PASSWORD = "REDDIT_PASSWORD"
+const CLIENT_ID = "CLIENT_ID"
+const CLIENT_SECRET = "CLIENT_SECRET"
+const USER_AGENT_NAME = "USER_AGENT_NAME"
+const URL = "URL"
+const CATEGORY = "CATEGORY"
+const NUM_OF_POSTS = "NUM_OF_POSTS"
+
+type SubredditFetcherConfig struct {
+	RedditUsername string
+	RedditPassword string
+	ClientID       string
+	ClientSecret   string
+	UserAgentName  string
+	URL            string
+	Category       string
+	NumOfPosts     int
+}
+
+func GetSubredditFetcherConfig() (*SubredditFetcherConfig, error) {
+	var err error
+	redditUsername := os.Getenv(REDDIT_USERNAME)
+	redditPassword := os.Getenv(REDDIT_PASSWORD)
+	clientID := os.Getenv(CLIENT_ID)
+	clientSecret := os.Getenv(CLIENT_SECRET)
+	userAgentName := os.Getenv(USER_AGENT_NAME)
+	url := os.Getenv(URL)
+	category := os.Getenv(CATEGORY)
+	numOfPosts := os.Getenv(NUM_OF_POSTS)
+	numOfPostsInt, err := strconv.Atoi(numOfPosts)
+	if err != nil {
+		return nil, err
+	}
+
+	return &SubredditFetcherConfig{RedditUsername: redditUsername, RedditPassword: redditPassword, ClientID: clientID, ClientSecret: clientSecret, UserAgentName: userAgentName, URL: url, Category: category, NumOfPosts: numOfPostsInt}, err
 
 }
--- reddit-api/reddit-api-fetcher/internal/fetcher/fetcher.go
+++ reddit-api/reddit-api-fetcher/internal/fetcher/fetcher.go
@@ -1,7 +1,46 @@
 package fetcher
 
+import (
+	"context"
+	"encoding/base64"
+	"net/http"
+	"net/url"
+	"reddit-api-fetcher/internal/config"
+	"strings"
+)
 
-type SubredditFetcher struct {
-	
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
 }
 
+type SubredditFetcher struct {
+	encodedData   string
+	basicAuth     string
+	UserAgentName string
+	URL           string
+	Category      string
+	NumOfPosts    int
+}
+
+func (s *SubredditFetcher) FetchToken(ctx context.Context, body string) {
+	req, err := http.NewRequest("POST", "https://www.reddit.com/api/v1/access_token", strings.NewReader(s.encodedData))
+
+	req.Header.Add("Authorization", "Basic "+s.basicAuth)
+	req.Header.Add("User-Agent", s.UserAgentName)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+}
+
+func (s *SubredditFetcher) FetchPosts(ctx context.Context, body string) {
+
+}
+
+func GetSubredditFetcher(config *config.SubredditFetcherConfig) *SubredditFetcher {
+	data := url.Values{}
+	data.Set("grant_type", "password")
+	data.Set("username", config.RedditUsername)
+	data.Add("password", config.RedditPassword)
+	encodedData := data.Encode()
+	basicAuth := basicAuth(config.ClientID, config.ClientSecret)
+
+}
