9f649b52b6798165d43c6712627167e44b3726e0
Petar Tisma <petar.tisma@outlook.com>
2023-05-10 14:01:58 +0200

    test new ver

--- api-server/cmd/api-server/main.go
+++ api-server/cmd/api-server/main.go
@@ -22,6 +22,7 @@ var (
 func init() {
 	// Test for GitHub actions, another test
 	// test3
+	// test3 again
 	config, err := initializers.LoadConfig("./")
 	if err != nil {
 		log.Fatal("🚀 Could not load environment variables", err)
